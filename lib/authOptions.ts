cbc08d6f931696993fae8fa54f4042388da662f4
Xeno <admin@MacBook-Pro.local>
2025-08-06 22:41:58 +0700

    trace api

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -30,6 +30,8 @@ export const authOptions = {
             .eq("username", username)
             .single();
 
+          console.log(user)
+
           if (error || !user) {
             console.error(error);
             throw new Error("Username atau password salah.");
